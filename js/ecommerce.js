4a5046c220f4980230ac5f689a415cb3db4634d0
ulirodriguezz <ulirodrigueze@gmail.com>
2025-02-13 09:48:54 -0300

    Entrega F1.1

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -1,17 +1,32 @@
-let loggedName;
-const productAlert = () => {
-    Toastify({
-        text: `El producto fue agregado al carrito`,
-        duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "center", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "#655CC9;",
-        },
-      }).showToast();
+const productAlert = (error) => {
+    if(error == true){
+        Toastify({
+            text: `Limite de compra alcanzado`,
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "red",
+            },
+          }).showToast();
+    }else{
+        Toastify({
+            text: `El producto fue agregado al carrito`,
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#655CC9",
+            },
+          }).showToast();
+    }
+
 };
+let loggedName;
 let productsDB;
 function startEcommerce(){
     let cartDB = localStorage.getItem("cartDB");
@@ -62,13 +77,23 @@ function addButtonEvents(){
             if(!cart.some(p => p.id == prodID)){
                 product.quantity = 1;
                 cart.push(product);
-                productAlert();
+                productAlert(false);
             }else{
                
                 //IDEM comentario de arriba
                 let index = cart.findIndex(p => p.id == prodID);
-                cart[index].quantity += 1;
-                productAlert();
+                //Si el producto ya fue agregado 10 veces al carrito, no permito que se agregue nuevamente
+                //Podría hacerse con un if-else pero para cumplir con el uso de try-catch lo hice por medio de un error
+                try{
+                    if(cart[index].quantity == 10){
+                        throw new Error("Limite alcanzado");
+                    }
+                    cart[index].quantity += 1;
+                    productAlert(false);
+                }catch(e){
+                    productAlert(true);
+                }
+                
             }
             localStorage.setItem("cartDB",JSON.stringify(cart));
         }
@@ -86,17 +111,26 @@ Toastify({
     position: "left", // `left`, `center` or `right`
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
-      background: "linear-gradient(to right,rgb(140, 249, 104),rgb(35, 198, 43))",
+      background: "linear-gradient(to right,rgb(99, 209, 63),rgb(35, 198, 43))",
     },
   }).showToast();
 
-//Busco los productos a la API/BD
+//Busco los productos a la API/BD (Simulado con archivo JSON)
 fetch("../db/products.json")
-    .then(response => response.json())
-    .then(data => {    
-    productsDB = data;
-    renderProducts(productsDB);
-    })
+.then(response => response.json())
+.then(data => {    
+productsDB = data;
+renderProducts(productsDB);
+})
+.catch(() => {
+    let productContainer = document.getElementById("product-container");
+    let errorMsj = document.createElement("h2");
+    errorMsj.innerText = "Ocurrió un problema, intente nuevamente";
+    errorMsj.style.color ="#8077e8";
+    productContainer.appendChild(errorMsj);
+    productContainer.style.justifyContent = "center";
+});
+    
 startEcommerce();
 
     
